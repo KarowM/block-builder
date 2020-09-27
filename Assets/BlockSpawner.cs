08c675fc2eedb179c19fbb3b95ea205356ae3b80
karow <karow.foxx@gmail.com>
2020-09-27 21:05:01 +0100

    added block types, added block spawner

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] blocks;
+
+    void Start() {
+        spawnNext();    
+    }
+
+    public void spawnNext() {
+        int i = Random.Range(0, blocks.Length);
+
+        Instantiate(blocks[i], transform.position, Quaternion.identity);
+    }
+}
